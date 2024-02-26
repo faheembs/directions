91421d875cd9464b6e473e44ad995c46a8dc87ff
FaheemS <92022763+faheembs@users.noreply.github.com>
2024-02-26 15:33:45 +0500

    Update baseURL.js

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:8000/"
-// export const baseURL = "https://nice-fez-seal.cyclic.app/"
+// export const baseURL = "http://localhost:8000/"
+export const baseURL = "http://51.20.84.168:8000/";
